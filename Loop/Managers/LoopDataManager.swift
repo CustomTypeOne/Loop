3b54758fe74dc1d33e376ab73090423777c3b22f
Jon Fawcett <jonfawcett10@gmail.com>
2022-02-15 15:41:28 -0500

    Added patches for Dynamic PAF, Auto Strategy Switching, and Negative IOB Factor

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -1638,9 +1638,47 @@ extension LoopDataManager {
             }
 
             let dosingRecommendation: AutomaticDoseRecommendation?
+            
+            // Dynamic Application Factor and Strategy Switching
+        
+            // Set to the hard coded 0.4
+            var dynamicApplicationFactor = LoopConstants.bolusPartialApplicationFactor;
+            
+            let dynamicPAFEnabled = UserDefaults.standard.bool(forKey: "dynamicPAFEnabled")
+            let minPartialApplicationFactor = UserDefaults.standard.double(forKey: "minPAF")
+            let minPartialApplicationFactorBG = UserDefaults.standard.double(forKey: "minPAFThreshold")
+            let maxPartialApplicationFactor = UserDefaults.standard.double(forKey: "maxPAF")
+            let maxPartialApplicationFactorBG = UserDefaults.standard.double(forKey: "maxPAFThreshold")
+            
+            let dosingStrategyAutomationEnabled = UserDefaults.standard.bool(forKey: "dosingStrategyAutomationEnabled")
+            let dosingStrategyThreshold = UserDefaults.standard.double(forKey: "dosingStrategyThreshold")
+            
+            if (dynamicPAFEnabled) {
+                if (glucose.quantity > HKQuantity(unit : settings.glucoseUnit ?? .milligramsPerDeciliter, doubleValue: minPartialApplicationFactorBG) &&
+                    glucose.quantity < HKQuantity(unit : settings.glucoseUnit ?? .milligramsPerDeciliter, doubleValue: maxPartialApplicationFactorBG)){
+                    dynamicApplicationFactor = ((glucose.quantity.doubleValue(for: settings.glucoseUnit ?? .milligramsPerDeciliter) - minPartialApplicationFactorBG) * ((maxPartialApplicationFactor - minPartialApplicationFactor)/(maxPartialApplicationFactorBG - minPartialApplicationFactorBG)) + minPartialApplicationFactor)
+                } else if (glucose.quantity >= HKQuantity(unit : settings.glucoseUnit ?? .milligramsPerDeciliter, doubleValue: maxPartialApplicationFactorBG)) {
+                    dynamicApplicationFactor = maxPartialApplicationFactorBG
+                } else if (glucose.quantity <= HKQuantity(unit : settings.glucoseUnit ?? .milligramsPerDeciliter, doubleValue: minPartialApplicationFactorBG)) {
+                    dynamicApplicationFactor = minPartialApplicationFactorBG
+                }
+            }
+            
+            var switcherIsAB = false;
+            
+            if (dosingStrategyAutomationEnabled) {
+                if( glucose.quantity > HKQuantity(unit : settings.glucoseUnit ?? .milligramsPerDeciliter, doubleValue: dosingStrategyThreshold) && settings.dosingStrategy == .automaticBolus){
+                     switcherIsAB = true;
+                 } else {
+                     switcherIsAB = false;
+                 }
+            } else if (settings.dosingStrategy == .automaticBolus) {
+                switcherIsAB = true;
+            }
+            
 
-            switch settings.dosingStrategy {
-            case .automaticBolus:
+            switch switcherIsAB {
+            case true:
                 let volumeRounder = { (_ units: Double) in
                     return self.delegate?.loopDataManager(self, roundBolusVolume: units) ?? units
                 }
@@ -1652,14 +1690,14 @@ extension LoopDataManager {
                     sensitivity: insulinSensitivity!,
                     model: doseStore.insulinModelProvider.model(for: pumpInsulinType),
                     basalRates: basalRates!,
-                    maxAutomaticBolus: maxBolus! * LoopConstants.bolusPartialApplicationFactor,
-                    partialApplicationFactor: LoopConstants.bolusPartialApplicationFactor,
+                    maxAutomaticBolus: maxBolus! * dynamicApplicationFactor,
+                    partialApplicationFactor: dynamicApplicationFactor,
                     lastTempBasal: lastTempBasal,
                     volumeRounder: volumeRounder,
                     rateRounder: rateRounder,
                     isBasalRateScheduleOverrideActive: settings.scheduleOverride?.isBasalRateScheduleOverriden(at: startDate) == true
                 )
-            case .tempBasalOnly:
+            case false:
                 let temp = predictedGlucose.recommendedTempBasal(
                     to: glucoseTargetRange!,
                     at: predictedGlucose[0].startDate,
